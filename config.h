fdcbd2b09b727f703b45e760a50864822795aa94
DChaovII <DChaovII@outlook.com>
2020-11-23 23:16:32 +0900

    Update config.h

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "DejaVuSansMono Nerd Font:style=Book:pixelsize=14:antialias=true:autohint=true";
+static char *font = "DejaVu Sans Mono:style=Book:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 5;
 
 /*
